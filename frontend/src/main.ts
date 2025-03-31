9624c408124c89816ba8ef63b847a2b0ff8bb0e0
Joakim Helgodt <joakim.helgodt@gmail.com>
2025-03-31 16:09:32 +0200

    fix main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
 import { provideHttpClient } from "@angular/common/http";
-iewwimport { provideRouter } from "@angular/router";
+import { provideRouter } from "@angular/router";
 import { routes } from "./app/app.routes";
 
 bootstrapApplication(AppComponent, {
