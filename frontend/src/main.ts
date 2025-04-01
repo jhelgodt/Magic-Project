3a5b7acd445c39fbdd5453f5357fec03cf984353
Joakim Helgodt <joakim.helgodt@gmail.com>
2025-04-01 10:49:13 +0200

    Add hash routing to main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,16 +2,18 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
 import { provideHttpClient } from "@angular/common/http";
-import { provideRouter } from "@angular/router";
+import { provideRouter, withHashLocation } from "@angular/router";
 import { routes } from "./app/app.routes";
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideRouter(routes), // Add routing
+    provideRouter(routes, withHashLocation()), // Add routing
     {
       provide: "API_URL",
       useValue: "https://magic-project-ph0g.onrender.com/api/v1",
     },
   ],
 }).catch((err) => console.error(err));
+
+console.log("Routes:", routes);
