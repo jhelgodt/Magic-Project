2ba7a0d34f6dbf248c81c21f4a62246cca3954a8
Joakim Helgodt <joakim.helgodt@gmail.com>
2025-04-03 09:03:53 +0200

    chore: clean up app.appcomponent.ts from imports

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,23 +1,15 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { NavbarComponent } from "./components/navbar/navbar.component";
-import { DeckBuilderComponent } from "./components/deck-builder/deck-builder.component";
 import { CommonModule } from "@angular/common"; // Import CommonModule
-import { CardBuilderComponent } from "./components/card-builder/card-builder.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [
-    RouterOutlet,
-    NavbarComponent,
-    DeckBuilderComponent,
-    CommonModule,
-    CardBuilderComponent,
-  ],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "portfolio2";
+  title = "Magic Project";
 }
