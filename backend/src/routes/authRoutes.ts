747a02a63325e875c8a3e2a7734294862760e78e
Joakim Helgodt <joakim.helgodt@gmail.com>
2025-04-05 07:48:46 +0200

    feat: Add logout route to handle user session termination
    - Implemented a logout route using req.logout() to terminate user sessions.
    - Redirect users to the appropriate frontend URL after logout based on the environment (local or production).
    - Added error handling for logout failures.

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -3,6 +3,9 @@ import passport from "passport";
 
 const router = express.Router();
 const isProduction = process.env.NODE_ENV === "production";
+const frontendUrl = isProduction
+  ? "https://jhelgodt.github.io/Magic-Project" // Production frontend URL
+  : "http://localhost:4200/Magic-Project"; // Local frontend URL
 
 // Start Google OAuth login
 router.get(
@@ -15,12 +18,18 @@ router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
-    const frontendUrl = isProduction
-      ? "https://jhelgodt.github.io/Magic-Project" // Production frontend URL
-      : "http://localhost:4200/Magic-Project"; // Local frontend URL
-
     res.redirect(`${frontendUrl}`); //
   }
 );
+router.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      console.error("Logout error:", err); // Log the error for debugging
+      return res.status(500).json({ error: "Failed to log out" });
+    }
+    // Redirect to the frontend homepage
+    res.redirect(`${frontendUrl}`);
+  });
+});
 
 export default router;
